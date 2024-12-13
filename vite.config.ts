f59be080b7c73a3e034b1106e29f781e6768fc11
saijo.shota <saijo.shota.biz@gmail.com>
2024-12-14 03:48:05 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
-});
+}));
