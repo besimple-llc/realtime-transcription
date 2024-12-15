922686fbcbdf20b84c5d3f92fc6c6f3b25ce7f11
saijo.shota <saijo.shota.biz@gmail.com>
2024-12-16 01:26:48 +0900

    audio録音が正しく終了するように修正

--- app/routes/_app.rooms.$roomId/_hooks/useAudio.ts
+++ app/routes/_app.rooms.$roomId/_hooks/useAudio.ts
@@ -4,6 +4,7 @@ export const useAudio = () => {
   const [isRecording, setRecording] = useState(false);
   const audioContextRef = useRef<AudioContext | null>(null);
   const workletNodeRef = useRef<AudioWorkletNode | null>(null);
+  const mediaStreamRef = useRef<MediaStream | null>(null);
 
   const startRecording = async (onMessage: (buffer: ArrayBuffer) => void) => {
     // オーディオコンテキスト作成
@@ -14,8 +15,8 @@ export const useAudio = () => {
     await audioContext.audioWorklet.addModule("/worklet/speech-processor.js");
 
     // マイク入力取得
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const source = audioContext.createMediaStreamSource(stream);
+    mediaStreamRef.current = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const source = audioContext.createMediaStreamSource(mediaStreamRef.current);
 
     // AudioWorkletNode作成
     const workletNode = new AudioWorkletNode(audioContext, "speech-processor");
@@ -44,15 +45,31 @@ export const useAudio = () => {
   }
 
   const stopRecording = async () => {
-    // 接続解除処理
-    const workletNode = workletNodeRef.current;
-    workletNode?.disconnect();
-    workletNodeRef.current = null;
+    try {
+      // 接続解除処理
+      const workletNode = workletNodeRef.current;
+      if (workletNode) {
+        workletNode.disconnect();
+        workletNodeRef.current = null;
+      }
 
-    const audioContext = audioContextRef.current;
-    audioContext?.close();
-    audioContextRef.current = null;
+      const audioContext = audioContextRef.current;
+      if (audioContext) {
+        await audioContext.close();  // 非同期処理を待機
+        audioContextRef.current = null;
+      }
 
+      // MediaStreamの停止
+      const stream = mediaStreamRef.current;
+      if (stream) {
+        for (const track of stream.getTracks()) {
+          track.stop();
+        }
+        mediaStreamRef.current = null;
+      }
+    } catch (error) {
+      console.error('Error stopping the recording:', error);
+    }
     setRecording(false);
   }
 
