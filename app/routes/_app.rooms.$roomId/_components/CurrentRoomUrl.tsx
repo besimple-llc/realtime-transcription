1bd487be7d77388c59bfa2141a58705b952b2024
saijo.shota <saijo.shota.biz@gmail.com>
2024-12-15 14:47:32 +0900

    add copy url button

--- app/routes/_app.rooms.$roomId/_components/CurrentRoomUrl.tsx
+++ app/routes/_app.rooms.$roomId/_components/CurrentRoomUrl.tsx
@@ -1,4 +1,5 @@
-import {useLocation} from "react-router";
+import {useState} from "react";
+import {Link, useLocation} from "react-router";
 
 type Props = {
   baseUrl: string;
@@ -6,8 +7,22 @@ type Props = {
 
 export const CurrentRoomUrl = ({ baseUrl }: Props) => {
   const location = useLocation();
+  const url = `${baseUrl}${location.pathname}`
+  const [copied, setCopied] = useState(false);
+
+  const handleOnCopy = async () => {
+    await navigator.clipboard.writeText(url)
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  }
 
   return (
-    <p>Room URL: {`${baseUrl}${location.pathname}`}</p>
+    <div className="flex flex-row gap-2">
+      <p>Room URL: <Link to={url} target="_blank">{url}</Link></p>
+      <button type="button" className="border rounded p-2" onClick={handleOnCopy}>{copied ? "コピーしました" : "コピーする"}</button>
+    </div>
+
   )
 }
